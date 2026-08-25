1a94d7412d3c0360b10dc405bde5da90f400fc20
samuelhilber <sam.hilber@gmail.com>
2026-08-25 19:23:33 +0200

    test

--- SemsterProjekt/Form1.Designer.cs
+++ SemsterProjekt/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -40,19 +41,31 @@
             button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(413, 201);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(2121, 984);
+            Controls.Add(label1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
     }
 }
