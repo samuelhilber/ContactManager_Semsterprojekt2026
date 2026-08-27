b00712b929b4c55983e6f78dce494ee722290e67
samuelhilber <sam.hilber@gmail.com>
2026-08-27 19:39:29 +0200

    Anfang der Validierung

--- SemsterProjekt/Employee.cs
+++ SemsterProjekt/Employee.cs
@@ -1,25 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SemsterProjekt
 {
     internal class Employee : Person // die Klasse welche die Mitarbeiter Erstellt, erbt von Person
     {
+        private static int _nextEmployeeNumber = 1;
+
         public int EmployeeNumber { get; init; }  // nur bei der Erstellung setzbar
         public Job Job { get; set; }
         public string AhvNumber { get; set; } = string.Empty;
         public int ManagmentLevel { get; set; } = 0;
         public string Nationality { get; set; } = string.Empty;
         public int Employment { get; set; } = 100;
-        public DateOnly EntryDate { get; set; }
-        public DateOnly? ExitDate { get; set; }
+        private DateOnly _EntryDate;
+        private DateOnly? _ExitDate;
         public bool Trainee { get; set; } = false;
         public string Adressprivat { get; set; } = string.Empty;
         public int Plzprivat { get; set; }
         public string Residance { get; set; } = string.Empty;
         public string? Adressbuisness { get; set; } = string.Empty;
         public int? PlzBuisness { get; set; }
+
+        public Employee() // mit dem Konstruktor wird jedesmal die Mitarbeiter nummer hochgezählt
+        {
+            EmployeeNumber = _nextEmployeeNumber++; // wenn ich das Programm neustarte geht dieser verloren auch wenn die anderen Daten gesichert sind muss ich noch irgendwie fixen warten auf Marin seine persistenz
+        }
+
+        public DateOnly EntryDate // übernommen von Geburstdatum in Person
+        {
+
+            get => _EntryDate;
+            set
+            {
+                if (value > DateOnly.FromDateTime(DateTime.Today))
+                {
+                    throw new ArgumentException("Eintritts Datum darf nicht in der Zukunft liegen");
+                }
+                _EntryDate = value;
+            }
+        }
+
+        public DateOnly? ExitDate // übernommen von Geburstdatum in Person + darf 0 sein Angstellte werden ja nicht 5 Jahre davor wissen wasn sie kündingen
+        {
+
+            get => _ExitDate;
+            set
+            {
+                if (value == null)
+                {
+                    _ExitDate = null;
+                    return;
+                }
+
+                if (value < DateOnly.FromDateTime(DateTime.Today)) // 
+                {
+                    throw new ArgumentException("Austritts Datum darf nicht in der Verganenheit liegen");
+                }
+                _ExitDate = value;
+            }
+        }
+
+        public double LehrJahr() // berechnen von angezeigten Lehrjahre für die Lehrlinge
+        {
+            if (ExitDate == null)
+            {
+                throw new InvalidOperationException("Kein Austrissdatum vorhanden");
+            }
+            if (!Trainee)
+            {
+                throw new InvalidOperationException("Nur für Lehrlinge berechenbar.");
+
+            }
+            double tage = ExitDate.Value.DayNumber - EntryDate.DayNumber;
+            double jahre = tage / 365;
+            return Math.Floor(jahre);
+        }
     }
 
     public enum Job //Enum für Dropdown Abteilung/Anstellung
@@ -32,3 +90,4 @@ namespace SemsterProjekt
         Aussendienst
     }
 }
+
--- SemsterProjekt/Person.cs
+++ SemsterProjekt/Person.cs
@@ -1,19 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
 
 namespace SemsterProjekt
 {
     internal class Person //ist die Father class wovon Employee und Customer die selben Daten erben
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public DateOnly BirthDate { get; set; }
-        public string MobilePhone { get; set; } = string.Empty;
-        public string BusinessPhone { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        private string _FirstName = string.Empty;
+        private string _LastName = string.Empty;
+        private DateOnly _BirthDate;
+        private string _MobilePhone = string.Empty;
+        private string _BusinessPhone = string.Empty;
+        private string _Email = string.Empty;
         public bool IsActive { get; set; } = true;
         public bool IsDeleted { get; set; } = false;
 
+        public string FirstName
+        {
+            get => _FirstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) // wie im Name wird zuerst geprüft ob es Null ist und dann vor und nach folgende Leerzeichen hat 
+                {
+                    throw new ArgumentException("Vorname darf nicht leer sein.");
+                }
+                string cleaned = Regex.Replace(value, @"[0-9]", ""); // sucht den string nach den value hier 0-9 und weil wir "" machen ersetzt es durch nichts also werden sämtliche Zahlen rausgefiltert
+                _FirstName = cleaned.Trim();
+            }
+        }
+
+        public string LastName
+        {
+            get => _LastName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) // wie im Name wird zuerst geprüft ob es Null ist und dann vor und nach folgende Leerzeichen hat 
+                {
+                    throw new ArgumentException("Vorname darf nicht leer sein.");
+                }
+                string cleaned = Regex.Replace(value, @"[0-9]", ""); // sucht den string nach den value hier 0-9 und weil wir "" machen ersetzt es durch nichts also werden sämtliche Zahlen rausgefiltert 
+                _LastName = cleaned.Trim();
+            }
+        }
+
+        public DateOnly BirthDate
+        {
+            
+            get => _BirthDate;
+            set
+            {
+                if (value > DateOnly.FromDateTime(DateTime.Today))
+                {
+                    throw new ArgumentException("Geburtsdarum darf nicht in der Zukunft liegen");
+                }
+                _BirthDate = value;
+            }
+        }
+
+        public string MobilePhone // 089 123 12 12
+        {
+            get => _MobilePhone;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) 
+                {
+                    throw new ArgumentException("Mobiletelefonnummer darf nicht leer bleiben.");
+                }
+
+                string cleanedone = Regex.Replace(value, @"A-Z", ""); // Entfernt Buchstaben          
+                string cleaned = Regex.Replace(cleanedone, @"\s+", " ").Trim(); // ersetzt zuerst mehrere Leerzeichen und ersetzt diese druch ein leerzeichen und trim schneidet den start und das ende ab so ist eine Telefon Nummer nicht länger als 13
+                
+
+                if (cleaned.Length > 13 )
+                {
+                    throw new ArgumentException("Zu viele Ziffern verwendet");
+                }
+                _MobilePhone = cleaned;
+            }
+        }
+
+        public string BusinessPhone // 089 123 12 12
+        {
+            get => _BusinessPhone;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) //da diese Angabe Optional ist wird geschaut ob ausversehen ein leerzeichen gesetzt wird, ist das der fall wird der String.Empty gesetzt dadruch kann der Rest übersprungen werden 
+                {
+                    _BusinessPhone = string.Empty;
+                    return;
+                }
+
+                string cleanedone = Regex.Replace(value, @"a-zA-Z", ""); // Entfernt Buchstaben          
+                string cleaned = Regex.Replace(cleanedone, @"\s+", " ").Trim(); // ersetzt zuerst mehrere Leerzeichen und ersetzt diese druch ein leerzeichen und trim schneidet den start und das ende ab so ist eine Telefon Nummer nicht länger als 13
+
+
+                if (cleaned.Length > 13)
+                {
+                    throw new ArgumentException("Zu viele Ziffern verwendet");
+                }
+                _BusinessPhone = cleaned;
+            }
+        }
+
+        public string Email
+        {
+            get => _Email;
+            set
+            {
+                if (value == null) // Basic check ob es leer bleibt
+                {
+                    throw new ArgumentException("Email darf nicht leer bleiben");
+                }
+
+                try // mit der Class MailAddress wird von c# die validierung übernommen mit diesem try catch wird sicher gestellt das überhaupt etwas eingegeben werden darf darum zuerst ein String und erst danach der vergleich
+                {
+                    var mail = new MailAddress(value);
+
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException("Ungültiges E-Mail Format.");
+                }
+
+                _Email = value;
+            }
+        }
     }
 }
